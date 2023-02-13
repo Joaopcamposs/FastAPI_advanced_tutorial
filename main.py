3e1bdf81512e2cde3baa0f99abe1724654804d51
Joaopcamposs <joaopcamposs@outlook.com>
2023-02-13 15:49:40 -0300

    Parâmetros de consulta e validações de texto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Union
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 
@@ -40,11 +40,23 @@ async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
     return result
 
 
-# Quando você declara outros parâmetros na função que não fazem parte dos parâmetros da rota, esses parâmetros são
-# automaticamente interpretados como parâmetros de "consulta".
 @app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        regex="^fixedquery$",
+        deprecated=True,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 # FastAPI é esperto o suficiente para perceber que o parâmetro da rota item_id é um parâmetro da rota, e q não é,
